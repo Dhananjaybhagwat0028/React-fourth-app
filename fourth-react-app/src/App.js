ba5f5665c66f797eb9fc919a2b351c61bb6fa7b3
dhananjay bhagwat <dhananjaybhagwat777@gmail.com>
2023-06-18 01:04:28 +0530

    navlink using button

--- fourth-react-app/src/App.js
+++ fourth-react-app/src/App.js
@@ -1,4 +1,10 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 import MyTodo from "./pages/MyTodo";
 import MyRegistration from "./pages/MyRegistration";
 import MyNavigationLinks from "./pages/MyNavigationLinks";
@@ -8,6 +14,7 @@ function App() {
     <>
       <BrowserRouter>
         <MyNavigationLinks />
+
         <Routes>
           {/** 1 Route means 1 Page */}
           <Route path="/" element={<h1>Hello</h1>} />
@@ -17,12 +24,36 @@ function App() {
           <Route path="*" element={<h1>Page Not Found</h1>} />
         </Routes>
 
-        <MyNavigationLinks />
+        <MyNavigationLinksUsingButton />
       </BrowserRouter>
     </>
   );
 }
 
+function MyNavigationLinksUsingButton() {
+  const navigate = useNavigate();
 
+  let goToHomeAction = () => {
+    navigate("/home");
+  };
+  let goToTodoAction = () => {
+    navigate("/todo");
+  };
+  let goToRegistrationAction = () => {
+    navigate("/registration");
+  };
+
+  return (
+    <div>
+      <input type="button" value="Home" onClick={goToHomeAction} />
+      <input type="button" value="Todo" onClick={goToTodoAction} />
+      <input
+        type="button"
+        value="Registartion"
+        onClick={goToRegistrationAction}
+      />
+    </div>
+  );
+}
 
 export default App;
