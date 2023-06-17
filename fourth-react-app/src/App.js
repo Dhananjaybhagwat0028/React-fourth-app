c28bf44b3b67a6171374f17e9bb0a0c2f363487c
dhananjay bhagwat <dhananjaybhagwat777@gmail.com>
2023-06-17 08:53:52 +0530

    c1

--- fourth-react-app/src/App.js
+++ fourth-react-app/src/App.js
@@ -1,55 +1,37 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 function App() {
   return (
     <>
-      <h1>My Todo</h1>
-      <MyTodo />
+      <h1>Stateful List</h1>
+      <ListDemo />
     </>
   );
 }
 
-function MyTodo() {
-  let [todo, setTodo] = useState({ task: "", description: "" });
+function ListDemo() {
+  let [list, setList] = useState(["delhi"]);
 
-  let handleChnageTaskAction = (e) => {
-    let newTodo = { ...todo, task: e.target.value };
-    setTodo(newTodo);
-  };
+  let addItemAction = () => {
+    // RESTRICATED :: WE SHOULD NOT USE DOCUMENT OBJECT :: STRICT NO
+    let inputRef = document.querySelector("#id1");
+    let inputValue = inputRef.value;
 
-  let handleChangeDescriptionAction = (e) => {
-    // console.log(e.target);
-    let newTodo = { ...todo, description: e.target.value };
-    setTodo(newTodo);
-  };
+    // let newList = [...list, inputValue];
+    let newList = [inputValue, ...list];
+    setList(newList);
 
-  let addTodoAction = () => {
-    console.log(todo);
-    // TODO :: Save this do DB
+    inputRef.value = "";
   };
 
   return (
     <>
-    <div style={{display:"flex"}}>
+      <input type="text" id="id1" placeholder="Enter user input..." />
+      <input type="button" value="Add New Item" onClick={addItemAction} />
 
-      <input
-        className="form-control"
-        type="text"
-        placeholder="Enter task"
-        value={todo.task}
-        onChange={handleChnageTaskAction}
-        />
-
-      <textarea
-        className="form-control"
-        cols="30"
-        rows="3"
-        placeholder="Enter Description"
-        value={todo.description}
-        onChange={handleChangeDescriptionAction}></textarea>
-
-      <input type="button" value="Add Todo" onClick={addTodoAction} />
-    </div>
+      {list.map((item) => (
+        <h1>{item}</h1>
+      ))}
     </>
   );
 }
