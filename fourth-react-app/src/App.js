ce834ea8ce27c48cdbce711ccef3ef10676c7396
dhananjay bhagwat <dhananjaybhagwat777@gmail.com>
2023-06-17 23:03:51 +0530

    reg added

--- fourth-react-app/src/App.js
+++ fourth-react-app/src/App.js
@@ -1,10 +1,13 @@
 import { useRef, useState } from "react";
 import MyTodo from "./pages/MyTodo";
+import MyRegistration from "./pages/MyRegistration";
 function App() {
   return (
     <>
       <h1>My Todo</h1>
       <MyTodo />
+       <hr />
+      <MyRegistration />
     </>
   );
 }
